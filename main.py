880fbd43dd8ebf7d52dab22620b926cd1d9d7eb1
PRASHANTH REDDY <81088623+prshanthreddy@users.noreply.github.com>
2024-12-17 22:06:30 -0600

    add logging and error handling for data ingestion stage in main.py

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.datascience import logger
 
 logger.info("This is an info message")
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.initiate_data_ingestion()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
